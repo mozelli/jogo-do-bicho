f67c920c6b7d8d8ffa86396a6349390946c8430d
João Mozelli Neto <joaomozelli@hotmail.com>
2020-08-28 17:41:53 -0300

    separado por jogos

--- src/index2.js
+++ src/index2.js
@@ -1,12 +1,29 @@
 const puppeteer = require('puppeteer');
+//const fs = require('fs');
 
 organizeData();
 
 async function organizeData() {
-    const items = await scrape();
+    const premios = await scrape();
 
-    console.log(items);
-    
+    let jogo = [];
+    const jogos = [];
+
+    let counter = 0;
+
+    premios.map(premio => {
+        if(counter < 7) {
+            jogo.push(premio);
+            counter = counter + 1;
+        } else {
+            jogos.push(jogo);
+            jogo = [];
+            jogo.push(premio)
+            counter = 0;
+        }
+    });
+    jogos.push(jogo);
+    console.log(jogos);
 }
 
 async function scrape() {
@@ -17,32 +34,37 @@ async function scrape() {
     await page.goto('https://www.resultadofacil.com.br/resultado-do-jogo-do-bicho/MG');
 
     const result = await page.evaluate(() => {
-        const games = [];
-        let game = []
+        const premios = [];
+        let premio = []
         let validCounter = 0;
         let count = 0;
         document.querySelectorAll('div > table > tbody > tr td')
             .forEach((item) => {
                 if (count < 4){
-                    game.push(item.innerText);
+                    premio.push(item.innerText);
                     count = count + 1;
                 } else {
-                    games.push(game);
-                    game = [];
-                    game.push(item.innerText);
+                    premios.push(premio);
+                    premio = [];
+                    premio.push(item.innerText);
                     count = 1;
                 }
                 
             });
-        return games;
+        return premios;
     });
 
     browser.close();
+    
     return result;
 } 
-//let scrape = async () => {
-    
-//}
 
-
-//scrape().then((value) => console.log(value));
+/*
+/*fs.writeFile('jogos.json', result, (error) => {
+        if(error) {
+            console.error("Ocorreu um erro no filesystem: " + error);
+        } else {
+            console.log("Ok!!");
+        }
+    })
+*/
